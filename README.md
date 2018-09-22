b0c91103d6c9875c4213935bb7d9ac7e2a1db0e3
shotesmagotes <shota.makino@yahoo.com>
2018-09-22 12:22:03 -0700

    Extended README

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # sortedwalk
-Traverse the files of a root directory.
+Generate the files of a root directory like os.walk
+but in a sorted order, and with an expanded set of 
+functions.
+
+For example, you can retrieve the parent, child, 
+sibling, or cousin directories of the current 
+directory you are traversing over. 
+
+# Usage
+
+The object produced by sortedwalk is an iterator and
+should be used as such to generate the current directory,
+the directory names and file names of the current directory.
+```
+  walk = SortedWalk(input_dir, None)
+  for (dirpath, dirnames, filenames) in walk:
+    print(dirpath, filenames)
+```
+
+
--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
-from setuptools import setup
+import setuptools
 
-setup(name='sortedwalk',
-      version='0.1',
-      description='Recursively traverse files in a root directory.',
-      url='http://github.com/shotesmagotes/sortedwalk',
-      author='Shota Makino',
-      author_email='shota.makino@yahoo.com',
-      license='MIT',
-      packages=['sortedwalk'],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name='sortedwalk',
+  version='0.1',
+  description='Recursively traverse files in a root directory.',
+  url='http://github.com/shotesmagotes/sortedwalk',
+  author='Shota Makino',
+  author_email='shota.makino@yahoo.com',
+  license='MIT',
+  long_description=long_description,
+  packages=setuptools.find_packages(),
+  zip_safe=False
+)
